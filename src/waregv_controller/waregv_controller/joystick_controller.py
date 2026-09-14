16d48b662e4d112f8cb9ee655ba2d5a917a2d71b
Milon <milonpauljs@gmail.com>
2026-09-15 02:31:58 +0530

    feat(): added waregv_v2 with nav

--- src/waregv_controller/waregv_controller/joystick_controller.py
+++ src/waregv_controller/waregv_controller/joystick_controller.py
@@ -26,15 +26,15 @@ class JoystickController(Node):
         )
 
         # Configurable velocity scaling factors
-        self.declare_parameter('max_linear_vel', 0.5)
-        self.declare_parameter('max_angular_vel', np.pi)
+        self.declare_parameter('max_linear_vel', 1.0)
+        self.declare_parameter('max_angular_vel', 3.14)
    
         self.send_stop = True
         self.get_logger().info('JoystickController node has been initialized.')
 
     def joy_callback(self, msg: Joy):
         if len(msg.axes) < 2:
-            returns
+            return
         enable_button_pressed = msg.buttons[4] == 1 
 
         if not enable_button_pressed:
