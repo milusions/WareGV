6381db57c23c4327e04355e19d1d648105f6a893
Milon <milonpauljs@gmail.com>
2026-09-21 01:03:46 +0530

    update hardware

--- src/waregv_bringup/launch/hardware.launch.py
+++ src/waregv_bringup/launch/hardware.launch.py
@@ -19,10 +19,10 @@ def generate_launch_description():
     # Launch Arguments
     mapping_enable_arg = DeclareLaunchArgument(name="mapping_enable", default_value="true")
     navigation_enable_arg = DeclareLaunchArgument(name="navigation_enable", default_value="true")
-    max_linear_velocity_arg = DeclareLaunchArgument(name="max_linear_velocity", default_value="0.2")
-    max_angular_velocity_arg = DeclareLaunchArgument(name="max_angular_velocity", default_value=str(np.pi))
-    wheel_radius_arg = DeclareLaunchArgument(name="wheel_radius", default_value="0.0325")
-    wheel_base_arg = DeclareLaunchArgument(name="wheel_base", default_value="0.176")
+    max_linear_velocity_arg = DeclareLaunchArgument(name="max_linear_velocity", default_value="0.1")
+    max_angular_velocity_arg = DeclareLaunchArgument(name="max_angular_velocity", default_value=str(np.pi/4))
+    wheel_radius_arg = DeclareLaunchArgument(name="wheel_radius", default_value="0.0385")
+    wheel_base_arg = DeclareLaunchArgument(name="wheel_base", default_value="0.158")
     map_name_arg = DeclareLaunchArgument(name="map_name", default_value="small_warehouse")
 
     # Configurations
