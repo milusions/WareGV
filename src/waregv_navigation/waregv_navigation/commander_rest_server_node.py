61d4b9a03ffd61976a8b9f66395c62e116f7a541
Milon <milonpauljs@gmail.com>
2026-09-21 03:21:10 +0530

    fix file location

--- src/waregv_navigation/waregv_navigation/commander_rest_server_node.py
+++ src/waregv_navigation/waregv_navigation/commander_rest_server_node.py
@@ -21,8 +21,8 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
 # Import specialized modules
-from manual_drive import ManualDriveManager
-from autonomous_drive import AutonomousDriveManager
+from waregv_navigation.manual_drive import ManualDriveManager
+from waregv_navigation.autonomous_drive import AutonomousDriveManager
 
 app = FastAPI(title="Navigation Commander REST Server")
 api_node = None
