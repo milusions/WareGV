a42b69314acddf99099635d91d4e8c4d392ce964
Milon <milonpauljs@gmail.com>
2026-09-24 06:33:35 +0530

    update dashboard

--- src/waregv_dashboard/launch/dashboard.launch.py
+++ src/waregv_dashboard/launch/dashboard.launch.py
@@ -6,27 +6,15 @@ from launch_ros.actions.node import Node
 
 def generate_launch_description():
 
-    mapping_check_arg = DeclareLaunchArgument(name="mapping_enable", default_value='true')
-    navigation_check_arg = DeclareLaunchArgument(name="navigation_enable", default_value='true')
-    map_name_arg = DeclareLaunchArgument(name="map_name", default_value='small_warehouse')
-    use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value='true')
- 
-    commander_node = Node(
+   
+    dashboard_backend = Node(
         package="waregv_dashboard",
-        executable="commander",
-        parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}],
-        output="screen",
-    )
-    
-    commander_rest_server_node = Node(
-        package="waregv_dashboard",
-        executable="commander_rest_server",
+        executable="dashboard_backend",
         parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}],
         output="screen",
     )
 
     return LaunchDescription([
  
-        # commander_node,
-        # commander_rest_server_node
+     dashboard_backend
     ])
