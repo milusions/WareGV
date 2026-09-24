9d294b0337550a1042c4796e02bf8be788cc2560
Milon <milonpauljs@gmail.com>
2026-09-24 06:38:18 +0530

    update hardware

--- src/waregv_bringup/launch/hardware.launch.py
+++ src/waregv_bringup/launch/hardware.launch.py
@@ -144,6 +144,30 @@ def generate_launch_description():
             "use_sim_time": use_sim_time,
         }.items(),
     )
+    
+    waregv_dashboard_launch_file_path = os.path.join(
+            get_package_share_directory("waregv_dashboard"),
+            "launch",
+            "dashboard.launch.py",
+        )
+    waregv_dashboard = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(waregv_dashboard_launch_file_path),
+            launch_arguments={
+                "use_sim_time": use_sim_time,
+            }.items(),
+        )
+        
+    waregv_user_interfaces_launch_file_path = os.path.join(
+            get_package_share_directory("waregv_user_interfaces"),
+            "launch",
+            "user_interfaces.launch.py",
+        )
+    waregv_user_interfaces = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(waregv_user_interfaces_launch_file_path),
+            launch_arguments={
+                "use_sim_time": use_sim_time,
+            }.items(),
+        )
 
     rosbridge_node = IncludeLaunchDescription(
         FrontendLaunchDescriptionSource(
@@ -197,6 +221,7 @@ def generate_launch_description():
             waregv_controller,
             waregv_mapping,
             waregv_navigation,
-     
+     waregv_dashboard,
+     waregv_user_interfaces
         ]
     )
